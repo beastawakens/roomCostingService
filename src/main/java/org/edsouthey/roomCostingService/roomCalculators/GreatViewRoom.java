cc7b0a04151ae9b8a0c2f3476d6b36d69a87aa3d
Edward Southey <rugbyhead@Edwards-MacBook-Pro.local>
2014-11-02 15:27:56 +0000

    ES: getting Great View working correctly

--- src/main/java/org/edsouthey/roomCostingService/roomCalculators/GreatViewRoom.java
+++ src/main/java/org/edsouthey/roomCostingService/roomCalculators/GreatViewRoom.java
@@ -1,10 +1,19 @@
 package org.edsouthey.roomCostingService.roomCalculators;
 
+import org.edsouthey.roomCostingService.Facility;
+
 public class GreatViewRoom extends Room {
+	private static final Integer  GREAT_VIEW_PREMIUM = 2;
 
 	@Override
 	public Integer getCost() {
-		return 0;
+		Integer runningTotal = 0;
+
+		for (Facility facility: getFacilities()) {
+			runningTotal += facility.getCost() * GREAT_VIEW_PREMIUM;
+		}
+		
+		return runningTotal;
 	}
 
 }
--- src/test/java/org/edsouthey/roomCostingService/roomCalculators/GreatViewRoomTest.java
+++ src/test/java/org/edsouthey/roomCostingService/roomCalculators/GreatViewRoomTest.java
@@ -3,6 +3,7 @@ package org.edsouthey.roomCostingService.roomCalculators;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.is;
 
+import org.edsouthey.roomCostingService.Facility;
 import org.junit.Test;
 
 public class GreatViewRoomTest {
@@ -12,5 +13,30 @@ public class GreatViewRoomTest {
 		Room greatViewRoom = new GreatViewRoom();
 		
 		assertThat(greatViewRoom.getCost(), is(0));
+	}
+	
+	@Test
+	public void shouldReturnDifferentPricesForTheDifferentFacilities() throws Exception {
+		Room greatViewRoom = new GreatViewRoom().with(Facility.BREAKFAST);
+		assertThat(greatViewRoom.getCost(), is(10));
+
+		greatViewRoom = new GreatViewRoom().with(Facility.ENSUITE);
+		assertThat(greatViewRoom.getCost(), is(8));
+
+		greatViewRoom = new GreatViewRoom().with(Facility.INTERNET);
+		assertThat(greatViewRoom.getCost(), is(4));
+		
+		greatViewRoom = new GreatViewRoom().with(Facility.LATE_CHECKOUT);
+		assertThat(greatViewRoom.getCost(), is(2));
+		
+		greatViewRoom = new GreatViewRoom().with(Facility.POOL);
+		assertThat(greatViewRoom.getCost(), is(2));
+	}
+	
+	@Test
+	public void shouldSumUpThePartsAppropriately() throws Exception {
+		Room greatViewRoom = new GreatViewRoom().with(Facility.ENSUITE).and(Facility.BREAKFAST);
+		
+		assertThat(greatViewRoom.getCost(), is(18));
 	}
 }
